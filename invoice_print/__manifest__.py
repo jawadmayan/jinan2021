fa07fe5adb1158ef34755df4550ea31c5db1f3ee
Mohammed Nabeel CP <cpmohammednabeel@gmail.com>
2021-02-24 18:18:04 +0530

    universal discount and store No field changes

--- invoice_print/__manifest__.py
+++ invoice_print/__manifest__.py
@@ -17,10 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','sale','sale_margin','sale_enterprise'],
+    'depends': ['base','account','sale','sale_margin','sale_enterprise', 'sale_stock'],
     'sequence' :1,
     # always loaded
     'data': [
